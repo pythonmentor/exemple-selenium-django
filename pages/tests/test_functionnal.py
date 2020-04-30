c44cd55a7d6dafeced70066bce19e2a9449da589
Thierry Chappuis <thierry@chappuis.io>
2020-04-30 22:05:59 +0200

    Added support for firefox

--- pages/tests/test_functionnal.py
+++ pages/tests/test_functionnal.py
@@ -2,12 +2,41 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.contrib.auth import get_user_model
 from selenium import webdriver
 
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.headless = True
+
+firefox_options = webdriver.FirefoxOptions()
+chrome_options.headless = True
+
 
 class ChromeFunctionalTestCases(StaticLiveServerTestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver.get(self.live_server_url)
+
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window()
+
+        User = get_user_model()
+        User.objects.create_user(username="tchappui", password="openClassrooms.2020")
+        
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_user_can_connect_and_disconnect(self):
+        self.driver.find_element_by_css_selector('#button-login').click()
+        self.driver.find_element_by_css_selector('#id_username').send_keys("tchappui")
+        self.driver.find_element_by_css_selector('#id_password').send_keys("openClassrooms.2020")
+        self.driver.find_element_by_css_selector('#button-submit').click()
+        logout = self.driver.find_element_by_css_selector('#button-logout')
+        self.assertEqual(logout.text, "Déconnexion", "Disconnect button should be available.")
+
+
+
+class FirefoxFunctionalTestCases(StaticLiveServerTestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox(firefox_options=firefox_options)
         self.driver.get(self.live_server_url)
 
         self.driver.implicitly_wait(30)
