6358263f657e99e9ecc8fce2c9d74bed13ca6de5
Thierry Chappuis <tchappui@gmail.com>
2020-11-23 19:35:51 +0100

    Refactored example

--- tests/functional/test_chrome.py
+++ tests/functional/test_chrome.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.contrib.auth import get_user_model
 from selenium import webdriver
@@ -7,56 +9,15 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('window-size=1920x1080')
 
 
-firefox_options = webdriver.FirefoxOptions()
-firefox_options.headless = True
-
-
 class ChromeFunctionalTestCases(StaticLiveServerTestCase):
     """Functional tests using the Chrome web browser in headless mode."""
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.driver = webdriver.Chrome(chrome_options=chrome_options)
-        cls.driver.implicitly_wait(30)
-        cls.driver.maximize_window()
-
-    @classmethod
-    def tearDownClass(cls):
-        super().tearDownClass()
-        cls.driver.quit()
-
-    def setUp(self):
-        User = get_user_model()
-        User.objects.create_user(
-            username="tchappui", password="openClassrooms.2020"
+        cls.driver = webdriver.Chrome(
+            chrome_options=chrome_options,
         )
-
-    def test_user_can_connect_and_disconnect(self):
-        self.driver.get(self.live_server_url)
-        self.driver.find_element_by_css_selector('#button-login').click()
-        self.driver.find_element_by_css_selector('#id_username').send_keys(
-            "tchappui"
-        )
-        self.driver.find_element_by_css_selector('#id_password').send_keys(
-            "openClassrooms.2020"
-        )
-        self.driver.find_element_by_css_selector('#button-submit').click()
-        logout = self.driver.find_element_by_css_selector('#button-logout')
-        self.assertEqual(
-            logout.text,
-            "Déconnexion",
-            "Disconnect button should be available.",
-        )
-
-
-class FirefoxFunctionalTestCases(StaticLiveServerTestCase):
-    """Functional tests using the Firefox web browser in headless mode."""
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.driver = webdriver.Firefox(firefox_options=firefox_options)
         cls.driver.implicitly_wait(30)
         cls.driver.maximize_window()
 
